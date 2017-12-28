e1c16a85bbf8b25aab13e159e47cb909c7b420d7
ProjetMultis <multisprojet78@gmail.com>
2017-12-28 22:55:21 +0100

    modify page home

--- user/user.php
+++ user/user.php
@@ -3,5 +3,21 @@ session_start();
 require "MVC/controller/controller.php";
 require "loadTwig.php";
 
-$templateIndex = $twig->loadTemplate('user.html.twig');
-echo $templateIndex->render(array());
+//retrivial data a page
+$page = isset($_GET['page']) ? $_GET['page'] : 0;
+
+//switch in page
+switch($page) {
+    case 1:
+    break;
+    case 2:
+    break;
+    case 3:
+    break;
+    case 4:
+    break;
+    default:
+        $templateIndex = $twig->loadTemplate('user.html.twig');
+        echo $templateIndex->render(array());
+}
+
